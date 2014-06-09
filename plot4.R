8e6419bf670619be47147a9ac194ebe77edeba62
svohra <svohra1969@gmail.com>
2014-06-08 20:10:45 -0400

    sixth commit

--- plot4.R
+++ plot4.R
@@ -5,7 +5,7 @@ times <- targetData[ ,2]
 x <- as.POSIXct(paste(dates, times))
 y <- targetData[ ,3]
 
-par(mfcol = c(2,4))
+par(mfcol = c(2,2))
 
 plot(x, y, type = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
 lines(x, y, type = "l")
@@ -18,6 +18,15 @@ lines(x, y1, type = "l", col = "black")
 lines(x, y2, type = "l", col = "red")
 lines(x, y3, type = "l", col = "blue")
 legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+y4 <- targetData[ ,5]
+plot(x, y4, type = "n", xlab = "datetime", ylab = "Voltage")
+lines(x, y4, type = "l")
+
+y5 <- targetData[ ,4]
+plot(x, y5, type = "n", xlab = "datetime", ylab = "Global_reactive_power")
+lines(x, y5, type = "l")
+
 
 dev.copy(png, file = "plot4.png")
 dev.off()
